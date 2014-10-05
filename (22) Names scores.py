3034b7b7aef6664742762bd0980975cc0c0e1351
Cameron Hillman <superc@hillman.id.au>
2014-10-05 15:02:25 +1100

    Added (22)

--- (22) Names scores.py
+++ (22) Names scores.py
@@ -1,19 +1,22 @@
-names = open("(22) names.txt").read().split("\",\"")
-names[0], names[5162] = "MARY", "ALONSO"
+letters = {"A": 1,  "B": 2,  "C": 3,
+           "D": 4,  "E": 5,  "F": 6,
+           "G": 7,  "H": 8,  "I": 9,
+           "J": 10, "K": 11, "L": 12,
+           "M": 13, "N": 14, "O": 15,
+           "P": 16, "Q": 17, "R": 18,
+           "S": 19, "T": 20, "U": 21,
+           "V": 22, "W": 23, "X": 24,
+           "Y": 25, "Z": 26}
+
+names = open("(22) names.txt").read()
+names = names.replace('"', "").split(",")
 names.sort()
-total = 0
-alphabet = {'M': 13, 'L': 12, 'O': 15, 'N': 14,
-            'I': 9, 'H': 8, 'K': 11, 'J': 10,
-            'E': 5, 'D': 4, 'G': 7, 'F': 6,
-            'A': 1, 'C': 3, 'B': 2, 'Y': 25,
-            'X': 24, 'Z': 26, 'U': 21, 'T': 20,
-            'W': 23, 'V': 22, 'Q': 17, 'P': 16,
-            'S': 19, 'R': 18}
 
+scores = []
 for name in names:
-    alph = 0
-    for letter in list(name):
-        alph += alphabet.get(letter)
-    total += alph * (names.index(name) + 1)
+  score = 0
+  for letter in list(name):
+    score += letters[letter]
+  scores.append(score)
 
-print(total)
+print(sum([a*b for (a, b) in list(enumerate(scores, 1))]))
